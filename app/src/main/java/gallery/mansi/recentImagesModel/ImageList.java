bfc48b8cd7dadd4dd1bfc29a8859f6029c909ac2
MANSI <mansig038>
2018-10-14 20:27:08 +0530

    search commit on one api

---
+++ app/src/main/java/gallery/mansi/recentImagesModel/ImageList.java
@@ -0,0 +1,31 @@
+package gallery.mansi.recentImagesModel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class ImageList {
+
+    @SerializedName("photos")
+    @Expose
+    private Photos photos;
+    @SerializedName("stat")
+    @Expose
+    private String stat;
+
+    public Photos getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Photos photos) {
+        this.photos = photos;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+}
